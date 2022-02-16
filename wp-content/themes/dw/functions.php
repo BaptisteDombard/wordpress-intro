b8357d9b2b8274800fa5e404dc080fa20eaebb58
BaptisteDombard <91158751+BaptisteDombard@users.noreply.github.com>
2022-02-16 12:06:46 +0100

    added trips custom post type

--- wp-content/themes/dw/functions.php
+++ wp-content/themes/dw/functions.php
@@ -4,3 +4,16 @@ add_filter('use_block_editor_for_post', '__return_false');
 
 //Activer les images sur les articles
 add_theme_support( 'post-thumbnails' );
+
+// enregistrer un seul custom post type pour "nos voyages"
+register_post_type('trip', [
+    'label' => 'Voyages',
+    'labels' => [
+        'name' => 'Voyages',
+        'singular_name' => 'Voyage'
+    ],
+    'description' => 'Tous les articles qui décrive un voyage',
+    'public' => true,
+    'menu_position' => 10,
+    'menu_icon' => 'dashicons-palmtree'
+]);
