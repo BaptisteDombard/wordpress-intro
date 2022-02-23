6070b58fa00b131b889b1c91029ba050b6777d7d
BaptisteDombard <91158751+BaptisteDombard@users.noreply.github.com>
2022-02-23 16:38:21 +0100

    Added contact form to contact template

--- wp-content/themes/dw/template-contact.php
+++ wp-content/themes/dw/template-contact.php
@@ -3,12 +3,15 @@
 <?php get_header(); ?>
 <?php if (have_posts()): while (have_posts()):the_post(); ?>
     <main class="layout contact">
-        <h2 class="contact__title"><?php get_the_title()?></h2>
+        <h2 class="contact__title">Contactez-moi</h2>
         <figure class="contact__fig">
             <?= get_the_post_thumbnail(null, 'medium_large', ['class' => 'singleTrip__thumbnail'])?>
         </figure>
         <div class="contact__content">
             <?php the_content();?>
+        </div>
+        <div class="contact__form">
+            <?= apply_filters('the_content', '[contact-form-7 id="40" title="Formulaire de contact"]')?>
         </div>
     </main>
 <?php endwhile; endif;?>
